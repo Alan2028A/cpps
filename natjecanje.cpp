408302cf2d5d95a757d9901fce9041b010186bc1
Alan2028A <alan.hudik28gmail.com>
2022-06-29 21:03:02 +0200

    fixed bug where numbers smaller than 0 occur.

--- natjecanje.cpp
+++ natjecanje.cpp
@@ -8,11 +8,15 @@ int main()
 	cout << "Unesite cijenu: ";
 	cin >> cijena;
 	int sati;
+	int minute; 
+	do {
 	cout << "Unesite sate: ";
 	cin >> sati;
-	int minute;
+	} while (sati>23);
+	do {
 	cout << "Unesite minute: ";
 	cin >> minute;
+	} while (minute>59);
 	cout << cijena-(sati+minute) <<endl;
 	return 0;
 }
